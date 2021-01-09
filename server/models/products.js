6230743b4cd6cc05a2c8776a2fe4054d47438762
Dagem Sinke <dagemsinke@gmail.com>
2021-01-09 14:09:16 -0500

    the product model

--- server/models/products.js
+++ server/models/products.js
@@ -1,1 +1,17 @@
 const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+        productName: String,
+        price: { type: Int, require:true},
+        category: String,
+        description: String 
+});
+module.exports = mongoose.model("Product", ProductSchema);
+
+db.product.insertOne(
+        { "productName" :  "Dog food",
+          "price" : 12 ,
+          "category" : "dog",
+          "description" : "a dog food"
+        }
+     )
